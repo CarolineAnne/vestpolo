4b406740fe66798109dc3a6f72845b4ad32db91b
Anne Caroline <annemoura2015@gmail.com>
2026-05-18 09:33:31 -0300

    Melhora admin dos pedidos

--- loja/admin.py
+++ loja/admin.py
@@ -4,22 +4,70 @@ from .models import Produto, Pedido, ItemPedido
 
 @admin.register(Produto)
 class ProdutoAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'preco', 'tamanho')
+    list_display = ('id', 'nome', 'preco', 'tamanho')
     search_fields = ('nome', 'descricao')
     list_filter = ('tamanho',)
+    list_per_page = 20
 
 
 class ItemPedidoInline(admin.TabularInline):
     model = ItemPedido
     extra = 0
     readonly_fields = ('produto', 'quantidade', 'subtotal')
+    fields = ('produto', 'quantidade', 'subtotal')
     can_delete = False
 
 
 @admin.register(Pedido)
 class PedidoAdmin(admin.ModelAdmin):
-    list_display = ('id', 'nome_cliente', 'telefone', 'status', 'total', 'data_pedido')
+    list_display = (
+        'id',
+        'nome_cliente',
+        'telefone',
+        'forma_entrega',
+        'status',
+        'total',
+        'data_pedido',
+    )
+
+    list_editable = ('status',)
+
+    list_filter = (
+        'status',
+        'forma_entrega',
+        'data_pedido',
+    )
+
+    search_fields = (
+        'id',
+        'nome_cliente',
+        'telefone',
+        'observacao',
+    )
+
     readonly_fields = ('data_pedido',)
-    list_filter = ('status', 'data_pedido')
-    search_fields = ('id',)
+
+    fieldsets = (
+        ('Dados do cliente', {
+            'fields': (
+                'nome_cliente',
+                'telefone',
+                'forma_entrega',
+                'observacao',
+            )
+        }),
+
+        ('Dados do pedido', {
+            'fields': (
+                'total',
+                'status',
+                'data_pedido',
+            )
+        }),
+    )
+
     inlines = [ItemPedidoInline]
+
+    list_per_page = 20
+
+    ordering = ('-data_pedido',)
