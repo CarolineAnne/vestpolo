18e2820bf423ba1aee5798eb4c274ca31656f26d
Anne Caroline <annemoura2015@gmail.com>
2026-06-06 13:49:11 -0300

    Configura envio real de email para redefinicao de senha

--- loja/urls.py
+++ loja/urls.py
@@ -1,4 +1,5 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
@@ -21,4 +22,40 @@ urlpatterns = [
     path('personalizados/', views.personalizados, name='personalizados'),
     path('universitarios/', views.universitarios, name='universitarios'),
     path('empresariais/', views.empresariais, name='empresariais'),
+
+    path(
+        'esqueci-senha/',
+        auth_views.PasswordResetView.as_view(
+            template_name='loja/esqueci_senha.html',
+            email_template_name='loja/email_recuperacao_senha.html',
+            subject_template_name='loja/assunto_recuperacao_senha.txt',
+            success_url=reverse_lazy('password_reset_done')
+        ),
+        name='password_reset'
+    ),
+
+    path(
+        'esqueci-senha/enviado/',
+        auth_views.PasswordResetDoneView.as_view(
+            template_name='loja/esqueci_senha_enviado.html'
+        ),
+        name='password_reset_done'
+    ),
+
+    path(
+        'redefinir-senha/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name='loja/redefinir_senha.html',
+            success_url=reverse_lazy('password_reset_complete')
+        ),
+        name='password_reset_confirm'
+    ),
+
+    path(
+        'senha-redefinida/',
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name='loja/senha_redefinida.html'
+        ),
+        name='password_reset_complete'
+    ),
 ]
